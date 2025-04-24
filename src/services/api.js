9ac143d9e603cceb2809425ef15e2e14163b25f4
Maicfer <mc.maicfer@gmail.com>
2025-04-23 22:40:58 -0500

    Configurar API con variable de entorno

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://tercerentrega-aiwn.onrender.com/api/";
+// Usa la variable de entorno definida en .env o en Render
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
 export default api;
+
+
